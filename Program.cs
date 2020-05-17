dfe6506c3e34f2e942f8184cbb021e08bcb2a2d3
Ifeadewumi <61970764+Ifeadewumi@users.noreply.github.com>
2020-05-17 18:15:03 +0100

    Second commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Text.RegularExpressions;
+using System;
 
 namespace NumberComparer
 {
@@ -6,7 +7,41 @@ namespace NumberComparer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int firstNumber;
+            int secondNumber;
+
+            Console.Write("Enter the first (positive) number: ");
+            firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second (positive) number: ");
+            secondNumber = int.Parse(Console.ReadLine());
+
+            if (firstNumber < 0 )
+            {
+                Console.WriteLine(firstNumber + " is a negative number. This program only takes in positive numbers.");
+            }
+            else if (secondNumber < 0)
+            {
+                Console.WriteLine(secondNumber + " is a negative number. This program only takes in positive numbers.");
+            }
+            else
+            {
+                if (firstNumber > secondNumber)
+                {
+                    Console.WriteLine(firstNumber + " with square root " + Math.Sqrt(firstNumber) + 
+                    " has a higher square root than " + secondNumber + " with square root " + Math.Sqrt(secondNumber) );
+                }
+                else if (firstNumber < secondNumber)
+                {
+                    Console.WriteLine(secondNumber + " with square root " + Math.Sqrt(secondNumber) + 
+                    " has a higher square root than " + firstNumber + " with square root " + Math.Sqrt(firstNumber) );
+                }
+                else
+                {
+                    Console.WriteLine(firstNumber + " with square root " + Math.Sqrt(firstNumber) + 
+                    " has the same square root than " + secondNumber + " with square root " + Math.Sqrt(secondNumber) );
+                }
+            }
         }
     }
 }
